83af18519d86dad0b31f6fbd9a43ce18e2e660e7
Gareth Hughes <g_github@flamescape.com>
2016-06-02 10:18:22 +0100

    es5 compatible

--- sage-fifty.js
+++ sage-fifty.js
@@ -1,5 +1,3 @@
-"use strict";
-
 var Promise = require('bluebird');
 var odbc = require('odbc');
 Promise.promisifyAll(odbc.Database.prototype);
@@ -8,121 +6,133 @@ var path = require('path');
 var Bridge = require('./bridge');
 var debug = require('debug')('sage-fifty');
 
-class Sage50 {
+function Sage50(options) {
+    if (!options) {
+        throw Error("Missing configuration options");
+    }
 
-	constructor (options) {
-		if (!options) {
-			throw Error("Missing configuration options");
-		}
+    if (!options.accdata) {
+        throw Error("ACCDATA path required");
+    }
 
-		if (!options.accdata) {
-			throw Error("ACCDATA path required");
-		}
+    if (!path.win32.isAbsolute(options.accdata)) {
+        throw Error("ACCDATA must be an absolute path");
+    }
 
-		if (!path.win32.isAbsolute(options.accdata)) {
-			throw Error("ACCDATA must be an absolute path");
-		}
+    if (!options.username) {
+        throw Error("Username required");
+    }
 
-		if (!options.username) {
-			throw Error("Username required");
-		}
+    if (!options.version) {
+        throw Error("Sage accounts data version must be specified");
+    }
 
-		if (!options.version) {
-			throw Error("Sage accounts data version must be specified");
-		}
+    this._accdata = options.accdata;
+    this._username = options.username;
+    this._password = options.password || "";
+    this._version = options.version;
 
-		this._accdata = options.accdata;
-		this._username = options.username;
-		this._password = options.password || "";
-		this._version = options.version;
+    debug("Sage50 linked to", this._accdata);
+}
 
-		debug("Sage50 linked to", this._accdata);
-	}
+/**
+ * Mark splits as paid in full
+ * @param  {object} options {}
+ * @return {Promise}        result object
+ */
+Sage50.prototype.payInFull = function(options) {
+    if (!options) {
+        throw Error("Missing configuration options");
+    }
 
+    if (!options.bankCode) {
+        throw Error("A bank code is required");
+    }
 
-	/**
-	 * Mark splits as paid in full
-	 * @param  {object} options {}
-	 * @return {Promise}        result object
-	 */
-	payInFull (options) {
-		if (!options) {
-			throw Error("Missing configuration options");
-		}
+    if (!options.reference) {
+        throw Error("A remittance reference is required");
+    }
 
-		if (!options.bankCode) {
-			throw Error("A bank code is required");
-		}
+    if (!options.accountRef) {
+        throw Error("A customer account reference is required");
+    }
 
-		if (!options.reference) {
-			throw Error("A remittance reference is required");
-		}
+    if (!options.splits || !options.splits.length) {
+        throw Error("An array of splits is required and must not be empty");
+    }
 
-		if (!options.accountRef) {
-			throw Error("A customer account reference is required");
-		}
+    return Bridge({
+        action       : "PayInFull",
+        accdata      : this._accdata,
+        username     : this._username,
+        password     : this._password,
+        bankCode     : options.bankCode,
+        reference    : options.reference,
+        details      : "Sales Receipt",
+        accountRef   : options.accountRef,
+        splits       : options.splits
+    }, options.onprogress);
+}
 
-		if (!options.splits || !options.splits.length) {
-			throw Error("An array of splits is required and must not be empty");
-		}
+Sage50.prototype.getSplitsByRange = function(options) {
+    return Bridge({
+        action       : "GetSplitsByRange",
+        accdata      : this._accdata,
+        username     : this._username,
+        password     : this._password,
+        start        : options.start || 1,
+        count        : options.count || 1000
+    }, options.onprogress).then(function(raw){
+        var headings = ["recordNumber", "details", "type", "tranNumber"];
+        var zipped = _.zip.apply(_, headings.map(function(head){ return raw[head]; }));
+        return zipped.map(function(line){
+            return _.zipObject(headings, line);
+        });
+    });
+}
 
-	    return Bridge({
-	    	action		 : "PayInFull",
-	        accdata      : this._accdata,
-	        username     : this._username,
-	        password     : this._password,
-	        bankCode     : options.bankCode,
-	        reference    : options.reference,
-	        details      : "Sales Receipt",
-	        accountRef   : options.accountRef,
-	        splits       : options.splits
-	    }, options.onprogress);
-	}
+Sage50.prototype.getSplits = function(options) {
+    return Bridge({
+        action       : "GetSplitsByRange",
+        accdata      : this._accdata,
+        username     : this._username,
+        password     : this._password,
+        splits       : options.splits
+    }, options.onprogress).then(function(raw){
+        var headings = ["recordNumber", "details", "type", "tranNumber"];
+        var zipped = _.zip.apply(_, headings.map(function(head){ return raw[head]; }));
+        return zipped.map(function(line){
+            return _.zipObject(headings, line);
+        });
+    });
+}
 
-	getSplitsByRange (options) {
-		return Bridge({
-	    	action		 : "GetSplitsByRange",
-	        accdata      : this._accdata,
-	        username     : this._username,
-	        password     : this._password,
-	        start 		 : options.start || 1,
-	        count 		 : options.count || 1000
-	    }, options.onprogress).then(function(raw){
-	    	var headings = ["recordNumber", "details", "type", "tranNumber"];
-	    	var zipped = _.zip.apply(_, headings.map(function(head){ return raw[head]; }));
-	    	return zipped.map(function(line){
-	    		return _.zipObject(headings, line);
-	    	});
-	    });
-	}
+Sage50.prototype._dbConnect = function() {
+    if (!!this._db) {
+        return;
+    }
 
-	_dbConnect () {
-		if (!!this._db) {
-			return;
-		}
+    this._odbc = odbc();
+    this._connStr = [
+        "Driver={Sage Line 50 v",
+        this._version,
+        "};DIR=",
+        this._accdata,
+        ";UseDataPath=No;UID=",
+        this._username,
+        ";PWD=",
+        this._password
+    ].join("");
 
-		this._odbc = odbc();
-		this._connStr = [
-	        "Driver={Sage Line 50 v",
-	        this._version,
-	        "};DIR=",
-	        this._accdata,
-	        ";UseDataPath=No;UID=",
-	        this._username,
-	        ";PWD=",
-	        this._password
-	    ].join("");
+    // connect to the odbc data
+    this._db = this._odbc
+        .openAsync(this._connStr)
+        .return(this._odbc);
+}
 
-	    // connect to the odbc data
-		this._db = this._odbc
-			.openAsync(this._connStr)
-			.return(this._odbc);
-	}
-
-	query (sql, params) {
-		this._dbConnect();
-		return this._db.call('queryAsync', sql, params);
-	}
+Sage50.prototype.query = function(sql, params) {
+    this._dbConnect();
+    return this._db.call('queryAsync', sql, params);
 }
 
 module.exports = Sage50;
